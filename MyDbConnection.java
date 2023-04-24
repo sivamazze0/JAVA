cc07ac51e07d14c5e1ea249bc553e2f90da16519
sivamazzeo <130139462+sivamazzeo@users.noreply.github.com>
2023-04-24 10:30:54 +0530

    Add files via upload
    Aprl 11 DB Connection

---
+++ MyDbConnection.java
@@ -0,0 +1,30 @@
+package com.dal.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDbConnection{
+	public static Connection con;
+
+	public static Connection getDbConnection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Sivamazzeo123");
+		} catch (ClassNotFoundException e) {
+		
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	   
+		
+		return con;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getDbConnection());
+	}
+
+}
